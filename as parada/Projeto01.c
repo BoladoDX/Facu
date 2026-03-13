7c3077ec53b7ea3fa62d57ca1186f72de6fccc3b
BoladoDX <viniciusirabemessias@gmail.com>
2026-03-13 15:24:21 -0400

    Add files via upload

---
+++ as parada/Projeto01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+
+    // declaração de variáveis
+    float nota01;
+
+    // entrada de dados
+    printf("\n Digite uma nota: ");
+    scanf("%f", &nota01);
+
+    // saída de dados
+    printf("\n A nota digitada foi: %.2f \n", nota01);
+    return 0;
+}
