bd641237c86acaf8e95de8c6a0dd340a8886f794
BoladoDX <viniciusirabemessias@gmail.com>
2026-03-20 17:52:03 -0400

    Add files via upload

---
+++ as parada/Exercícios Lista1/Exercício11Lista1.c
@@ -0,0 +1,24 @@
+//Joãozinho quer calcular e mostrar a quantidade de litros de combustível
+//gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L.
+//Para isso, ele gostaria que você o auxiliasse através de um simples programa.
+//Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas)
+//e a velocidade média durante a mesma (em km/h).
+//Assim, pode-se obter distância percorrida e, em seguida,
+//calcular quantos litros seriam necessários.
+//Mostre o valor com 3 casas decimais após o ponto.
+
+#include <stdio.h>
+
+int main(){
+
+    float horas, velocidade, lit_nec, distancia;
+
+    printf("Digite o tempo gasto na viagem e a velocidade media durante ela: \n");
+    scanf("%f %f", &horas, &velocidade);
+
+    distancia = (velocidade * horas);
+	lit_nec = (distancia / 12);
+	
+	printf("A distancia percorrida foi %.2f, logo, a quantidade de litros gastos foram de: %.3fL \n", distancia, lit_nec);
+	return 0;
+}
