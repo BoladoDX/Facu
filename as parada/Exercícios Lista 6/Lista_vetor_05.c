c95476d07da31657b0b7e5b89b69651068340897
BoladoDX <viniciusirabemessias@gmail.com>
2026-05-15 15:24:27 -0400

    Add files via upload

---
+++ as parada/Exercícios Lista 6/Lista_vetor_05.c
@@ -0,0 +1,10 @@
+//Dado um vetor A de tamanho 20 e do tipo inteiro faça um programa em C que, utilizando um laço de repetição,
+//insira os valores de 5 a 24 e, utilizando outro laço de repetição,
+//calcule o dobro de cada valor inserido e armazene em um vetor B.
+
+#include <stdio.h>
+
+int main()
+{
+    
+}
