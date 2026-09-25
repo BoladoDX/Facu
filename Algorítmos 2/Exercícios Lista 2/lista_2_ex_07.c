f8124da3d523538add7a449ee5f71001d777cd59
BoladoDX <viniciusirabemessias@gmail.com>
2026-09-24 20:54:17 -0400

    Add files via upload

--- Algorítmos 2/Exercícios Lista 2/lista_2_ex_07.c
+++ Algorítmos 2/Exercícios Lista 2/lista_2_ex_07.c
@@ -2,30 +2,50 @@
 //segunda string ocorre dentro da primeira. Use aritmética de ponteiros para acessar os
 //caracteres das strings.
 
+
 #include <stdio.h>
 
-int ocorre_string(char *string1, char *string2)
+int procurar_texto(char *palavra1, char *palavra2) 
 {
-    int i = 0;
+    
+    for (int i = 0; *palavra1 != '\0'; palavra1++) 
+    {
+        char *p1 = palavra1;
+        char *p2 = palavra2;
+      
+        while (*p1 == *p2 && *p2 != '\0') 
+        {
+            p1++;
+            p2++;
+        }
 
+        if (*p2 == '\0') 
+        {
+            return 1;
+        }
+    }
 
+    return 0;
 }
 
-int main()
+int main() 
 {
-    int res;
-    char str1[] = {"GENESIS\0"};
-    char str2[] = {"GENES\0"};
+    char string1[100];
+    char string2[100];
 
-    res = ocorre_string(str1, str2);
+    printf("Digite a primeira palavra:");
+    scanf("%s", string1); 
 
-    if(res == 1)
+    printf("Digite a segunda palavra:");
+    scanf("%s", string2); 
+
+    if (procurar_texto(string1, string2) == 1) 
     {
-        printf("A segunda string ocorre na primeira!\n");
-    }
-    else
+        printf("A Primeira String:  '%s' contem a Segunda String '%s'.\n", string1, string2);
+    } 
+    else 
     {
-        printf("A segunda string nao ocorre na primeira.\n");
+        printf("A Primeira String:  '%s' nao contem a Segunda String '%s'.\n", string1, string2);
     }
 
     return 0;
